f561e225bbc31865a9e86a8e1769a871d40f0d65
Rishi Raam <rishiraamns@gmail.com>
2025-01-29 14:47:22 +0530

    update cors

--- question/settings.py
+++ question/settings.py
@@ -121,3 +121,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# X-Frame-Options: Allow embedding from a specific URL
+X_FRAME_OPTIONS = 'ALLOW-FROM https://taptapdev.blackbucks.me'
+
+# CORS settings: Allow your iframe parent URL
+CORS_ALLOWED_ORIGINS = [
+    'https://taptapdev.blackbucks.me',  # Add your domain here
+]
